627b8b225a7efee6eb225fcb7c554db7f5970fe1
Sayem995 <sayem.bokhari.1@gmail.com>
2020-01-18 18:44:31 +0000

    created components folder within src folder and added products.js file to it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products';
 
-function App() {
+class App extends Component {
+ render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <h1>Ecommerce Shopping Cart Application</h1>
+    <hr/>
+    <div className="row">
+      <div className="col-md-8">
+        <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart}/>
+      </div>
+      <div className="col-md-4">
+
+      </div>
+     </div>
+
+     </div>
   );
+ }
 }
 
 export default App;
