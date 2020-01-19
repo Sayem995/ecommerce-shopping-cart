a12b3c57173354571dea3546e26af870b7d68e5d
Sayem995 <sayem.bokhari.1@gmail.com>
2020-01-19 15:39:43 +0000

    added Products.js file to components folder, created constructor in App class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Products from './components/Products';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Products from "./components/Products";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { products:[], filteredProducts: [], }
+  }
  render() {
   return (
     <div className="container">
